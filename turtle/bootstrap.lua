26f7373b846d416178291a2a76721623be9e1cf3
Bas Bouwhuis <basbouwhuis@protonmail.com>
2025-07-20 11:24:39 +0200

    Bootstrap script

---
+++ turtle/bootstrap.lua
@@ -0,0 +1,24 @@
+local base = "https://raw.githubusercontent.com/rahmerh/turtle-manager/main"
+
+local files = {
+    ["main.lua"] = "turtle/main.lua",
+    ["startup.lua"] = "turtle/startup.lua",
+}
+
+for dest, src in pairs(files) do
+    local url = base .. "/" .. src
+    print("Downloading " .. src .. " → " .. dest)
+
+    local response = http.get(url)
+    if response then
+        fs.makeDir(fs.getDir(dest))
+        local f = fs.open(dest, "w")
+        f.write(response.readAll())
+        f.close()
+        response.close()
+    else
+        print("Failed to fetch " .. url)
+    end
+end
+
+print("✓ All files downloaded. Ready to run.")
